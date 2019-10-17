f17cc8f33f034e044b6396c67a6df13fa707d341
Rafal Kaniewski <rafal.kaniewski@gmail.com>
2019-10-17 12:30:23 +0200

    Added Person class in classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,19 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  function(foodEat) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(foodEat);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
+}
